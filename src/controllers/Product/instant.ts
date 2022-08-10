9410ed8958966d6c50169d1f031c9d7284f1f2ed
Wallace <wbescrovaine@gmail.com>
2022-08-10 18:49:04 -0300

    adding

--- src/controllers/Product/instant.ts
+++ src/controllers/Product/instant.ts
@@ -43,12 +43,8 @@ export class Product {
       // desestrutura dados do corpo da requisição
       const {
         name,
-        description,
-        value,
       }: {
         name: string;
-        description: string;
-        value: number;
       } = request['body'];
       // Verifica os dados recebidos
       if (!name) {
@@ -60,13 +56,89 @@ export class Product {
       }
 
       const findProduct = await ProductModel.findOne({
-        where:{name}
+        where: { name },
       });
 
       return response.status(200).json({
         status: 'Success',
         message: 'Product find',
         obg: findProduct,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async update(request: Request, response: Response) {
+    try {
+      console.info('Atualizando');
+      // desestrutura dados do corpo da requisição
+      const {
+        name,
+        newName,
+        description,
+        value,
+      }: {
+        name: string;
+        newName: string;
+        description: string;
+        value: number;
+      } = request['body'];
+      // Verifica os dados recebidos
+      if (!name) {
+        // Em caso de erro
+        return response.status(400).json({
+          status: 'Error',
+          message: 'Enter correct input',
+        });
+      }
+      const findProduct = await ProductModel.findOne({
+        where: { name },
+      });
+
+      const updateProductValues = {
+        name: newName,
+        description,
+        value,
+      };
+
+      const output = await findProduct?.update(updateProductValues, {
+        where: { name },
+      });
+
+      return response.status(200).json({
+        status: 'Success',
+        message: 'Product updated',
+        obg: output,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async delete(request: Request, response: Response) {
+    try {
+      console.info('Deletando');
+      // desestrutura dados do corpo da requisição
+      const {
+        name,
+      }: {
+        name: string;
+      } = request['body'];
+      // Verifica os dados recebidos
+      if (!name) {
+        // Em caso de erro
+        return response.status(400).json({
+          status: 'Error',
+          message: 'Enter correct input',
+        });
+      }
+      const deleteProduct = await ProductModel.destroy({
+        where: { name },
+      });
+
+      return response.status(200).json({
+        status: 'Success',
+        message: 'Product updated',
+        obg: deleteProduct,
       });
     } catch (error) {
       console.error(error);
